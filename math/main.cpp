1e8d3d80bd4f3829219c8c603ab5c9c0ebd0ab19
Patryk Kups <patryk.kups.priv@gmail.com>
2026-01-07 22:02:13 +0100

    math exercises 2

--- math/main.cpp
+++ math/main.cpp
@@ -1,15 +1,18 @@
-#include <cmath>
-#include <iostream>
-#include <cstdint>
-#include <numeric>
+#include <algorithm>
 #include <array>
+#include <cmath>
+#include <cstdint>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <vector>
 
 constexpr uint64_t sumOfNumbersDivisibleBy3or5(uint32_t number)
 {
     uint64_t sum{};
-    for(uint32_t i = 3; i <= number; i++)
+    for (uint32_t i = 3; i <= number; i++)
     {
-        if(i % 3 == 0 || i % 5 == 0)
+        if (i % 3 == 0 || i % 5 == 0)
         {
             sum += i;
         }
@@ -20,7 +23,7 @@ constexpr uint64_t sumOfNumbersDivisibleBy3or5(uint32_t number)
 constexpr uint64_t gcd_greatestCommonDivisor(uint64_t a, uint64_t b)
 {
     // GCD(a,b) = GCD(b, a%b)
-    while(b != 0)
+    while (b != 0)
     {
         a %= b;
         std::swap(a, b);
@@ -31,75 +34,188 @@ constexpr uint64_t gcd_greatestCommonDivisor(uint64_t a, uint64_t b)
 
 constexpr uint64_t lcm_leastCommonMultiple(uint64_t a, uint64_t b)
 {
-    if(a == 0 || b == 0) return 0;
+    if (a == 0 || b == 0)
+        return 0;
 
     // LCM(a,b) = a*b / GCD(a,b)
     // Use transformed formula to reduce the risk of integer overflow
-    // LCM(a,b) = (a/GDC(a,b)) * b 
+    // LCM(a,b) = (a/GDC(a,b)) * b
     return (a / gcd_greatestCommonDivisor(a, b)) * b;
 }
 
-template <typename InputIt>
-constexpr uint64_t lcm_group(InputIt begin, InputIt end)
+template <typename InputIt> constexpr uint64_t lcm_group(InputIt begin, InputIt end)
 {
     return std::accumulate(begin, end, 1, lcm_leastCommonMultiple);
 }
 
 constexpr bool isPrime(uint64_t n)
 {
-    if(n <= 3) return n>1;
-    if(n % 2 == 0 || n % 3 == 0) return false;
+    if (n <= 3)
+        return n > 1;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
     // algo 6k +- 1
-    for(uint64_t i = 5; i*i < n; i+=6)
+    for (uint64_t i = 5; i * i < n; i += 6)
     {
-        if(n % i == 0 || n % (i+2) == 0) return false;
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
     }
     return true;
 }
 constexpr uint64_t firstPrimeNumberBefore(uint64_t n)
 {
-    while(n != 0)
+    while (n != 0)
     {
-        n--; 
-        if(isPrime(n)) return n;
+        n--;
+        if (isPrime(n))
+            return n;
     }
 
     return n;
 }
 
-void printPrime6kNumbersUntil(uint64_t n)
+constexpr void printPrime6kNumbersUntil(uint64_t n)
 {
-    if (n < 11) std::cout << "no prime 6k\n";
+    if (n < 11)
+        std::cout << "no prime 6k\n";
 
-    for(uint64_t i = 5; i <= n; i++)
+    for (uint64_t i = 5; i <= n; i++)
     {
-        if(isPrime(i) && isPrime(i+6))
+        if (isPrime(i) && isPrime(i + 6))
         {
-            std::cout << "Prime6k{" << i << "," << i+6 << "}" << std::endl;
+            std::cout << "Prime6k{" << i << "," << i + 6 << "}" << std::endl;
         }
     }
     std::cout << std::endl;
 }
 
-void calcAbundantNumbersUntil(uint64_t n)
+uint64_t sumOfDivisors(uint64_t i)
+{
+    uint64_t sumOfDivisors{1};
+    for (uint64_t j = 2; j <= std::sqrt(i); j++)
+    {
+        if (i % j == 0)
+        {
+            sumOfDivisors += (j == (i / j)) ? j : j + (i / j);
+        }
+    }
+    return sumOfDivisors;
+}
+
+std::pair<uint64_t, uint64_t> makeOrderPair(uint64_t a, uint64_t b)
+{
+    if (a > b)
+        return std::make_pair(b, a);
+    return std::make_pair(a, b);
+}
+
+constexpr void calcAbundantNumbersUntil(uint64_t n)
 {
     int counter = 1;
-    for(uint64_t i = 10; i <= n; i++)
+    for (uint64_t i = 10; i <= n; i++)
     {
-        uint64_t sumOfDivisors{1};
-        for(uint64_t j = 2; j <= std::sqrt(i); j++)
+        const auto sum = sumOfDivisors(i);
+        if (sum > i)
         {
-            if(i % j == 0)
-            {
-                sumOfDivisors += (j == (i/j)) ? j : j + (i/j);
-            }
-        }
-        if (sumOfDivisors > i)
-        {
-            std::cout << counter << " Abundant number(" << i << ") Abound=" << sumOfDivisors-i << std::endl;
+            std::cout << counter << " Abundant number(" << i << ") Abound=" << sum - i << std::endl;
             counter++;
         }
     }
+}
+
+void printAmicableUntil(uint64_t limit)
+{
+    std::set<std::pair<uint64_t, uint64_t>> friends;
+
+    for (uint64_t i = 0; i < limit; i++)
+    {
+        const auto sum1 = sumOfDivisors(i);
+        if (sum1 < limit)
+        {
+            const auto sum2 = sumOfDivisors(sum1);
+            if (i == sum2 && i != sum1)
+            {
+                friends.insert(makeOrderPair(i, sum1));
+            }
+        }
+    }
+
+    std::cout << "Amount=" << friends.size() << std::endl;
+    for (const auto i : friends)
+    {
+        std::cout << i.first << " and " << i.second << " are friends numbers" << std::endl;
+    }
+}
+
+constexpr void checkIfArmstrongNum(uint64_t n)
+{
+    uint32_t digits{};
+    uint64_t buffer = n;
+
+    while (buffer > 0)
+    {
+        buffer /= 10;
+        digits++;
+    }
+
+    buffer = n;
+    uint64_t sum{};
+    for (auto i = 0u; i < digits; i++)
+    {
+        uint64_t digit = buffer % 10;
+        sum = std::pow(digit, digits);
+        digit /= 10;
+    }
+
+    if (sum == n)
+    {
+        std::cout << n << " is Armstrong number\n";
+    }
+}
+
+void printPrimeFactors(uint64_t n)
+{
+    std::vector<uint64_t> vec;
+    while (n % 2 == 0)
+    {
+        vec.emplace_back(2);
+        n /= 2;
+    }
+
+    for (int i = 3; i < std::sqrt(n); i += 2)
+    {
+        while (n % i == 0)
+        {
+            vec.emplace_back(i);
+            n /= i;
+        }
+    }
+
+    if (n > 2)
+    {
+        vec.emplace_back(n);
+    }
+    for (const auto &n : vec)
+    {
+        std::cout << n << std::endl;
+    }
+}
+
+std::vector<bool> decToBin(uint64_t n)
+{
+    std::vector<bool> bin;
+
+    if (n == 0)
+    {
+        return {0};
+    }
+    while (n > 0)
+    {
+        bin.emplace_back(n%2);
+        n /= 2;
+    }
+    std::reverse(bin.begin(), bin.end());
+    return bin;
 }
 
 int main()
@@ -113,6 +229,13 @@ int main()
     // firstPrimeNumberBefore(100);
     // printPrime6kNumbersUntil(100);
     // calcAbundantNumbersUntil(200);
-    
+    // printAmicableUntil(1000000);
+    // printPrimeFactors(420);
+
+    for (const auto &i : decToBin(504))
+    {
+        std::cout << i;
+    }
+
     return 0;
 }
--- math/main.go
+++ math/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"sort"
 )
 
 func sumOfNumbersDivisibleBy3or5(number uint32) uint64 {
@@ -70,26 +71,153 @@ func printPrime6kNumbersUntil(n uint64) {
 	}
 }
 
+func sumOfDivisors(i uint64) uint64 {
+	var sumOfDivisors uint64 = 1
+	i_sqrt := uint64(math.Sqrt(float64(i)))
+
+	for j := uint64(2); j <= i_sqrt; j++ {
+		if i%j == 0 {
+			if j != i/j {
+				sumOfDivisors += j + i/j
+			} else {
+				sumOfDivisors += j
+			}
+		}
+	}
+	return sumOfDivisors
+}
+
 func calcAbundantNumbersUntil(n uint64) {
 	counter := 1
 	for i := uint64(10); i <= n; i++ {
-		var sumOfDivisors uint64 = 1
-		i_sqrt := uint64(math.Sqrt(float64(i)))
-
-		for j := uint64(2); j <= i_sqrt; j++ {
-			if i%j == 0 {
-				if j != i/j {
-					sumOfDivisors += j + i/j
-				} else {
-					sumOfDivisors += j
-				}
-			}
-		}
+		sumOfDivisors := sumOfDivisors(i)
 		if sumOfDivisors > i {
 			fmt.Println(counter, "Abundant number(", i, ") Abound=", sumOfDivisors-i)
 			counter++
 		}
 	}
+}
+
+type Pair struct {
+	first, second uint64
+}
+
+func makeOrderPair(a, b uint64) Pair {
+	if a > b {
+		return Pair{first: b, second: a}
+	}
+	return Pair{first: a, second: b}
+}
+
+func printAmicableUntil(limit uint64) {
+	set := make(map[Pair]struct{})
+	for i := uint64(4); i < limit; i++ {
+		sum1 := sumOfDivisors(i)
+		if sum1 < limit {
+			sum2 := sumOfDivisors(sum1)
+			if sum2 == i && sum1 != i {
+				set[makeOrderPair(i, sum1)] = struct{}{}
+			}
+		}
+	}
+	fmt.Println("Amount=", len(set))
+	var keys = make([]uint64, 0, len(set))
+	for v := range set {
+		keys = append(keys, v.first)
+		// fmt.Printf("%d and %d are friend numbers\n", v.first, v.second)
+	}
+	sort.Slice(keys, func(i, j int) bool {
+		return keys[i] < keys[j]
+	})
+	for _, v := range keys {
+		fmt.Printf("%d are friend\n", v)
+	}
+}
+
+func checkIfArmstrongNum(n uint64) {
+	temp := n
+	digits := 0
+	for temp > 0 {
+		temp /= 10
+		digits++
+	}
+
+	sum := float64(0)
+	temp = n
+	for i := 0; i < digits; i++ {
+		digit := temp % 10
+		sum += math.Pow(float64(digit), float64(digits))
+		temp /= 10
+	}
+
+	if uint64(sum) == n {
+		fmt.Println(n, "is Armstrong number")
+	}
+}
+
+func printPrimeFactors(n uint64) {
+	vec := []uint64{}
+	for n%2 == 0 {
+		vec = append(vec, 2)
+		n /= 2
+	}
+	for i := uint64(3); i <= uint64(math.Sqrt(float64(n))); i += 2 {
+		for n%i == 0 {
+			vec = append(vec, i)
+			n /= i
+		}
+	}
+
+	if n > 2 {
+		vec = append(vec, n)
+	}
+
+	for _, i := range vec {
+		fmt.Println(i)
+	}
+}
+
+func decToBin(n uint64) []byte {
+	bin := []byte{}
+	for n > 0 {
+		bin = append(bin, byte(n%2))
+		n /= 2
+	}
+	return bin
+}
+
+func gray_encode(n uint64) uint64 {
+	bin := decToBin(n)
+	grayEncoded := uint64(n ^ (n >> 1))
+	binGray := decToBin(grayEncoded)
+	fmt.Print("Value: ")
+	for i := len(bin) - 1; i > 0; i-- {
+		fmt.Print(bin[i])
+	}
+
+	fmt.Println()
+
+	fmt.Print("Gray encoded: ")
+	for i := len(binGray) - 1; i > 0; i-- {
+		fmt.Print(binGray[i])
+	}
+	fmt.Println()
+	return grayEncoded
+}
+
+func gray_decode(n uint64) uint64 {
+	decoded := uint64(0)
+	for i := n; i != 0; i >>= 1 {
+		decoded ^= i
+	}
+	bin := decToBin(decoded)
+
+	fmt.Print("Gray decoded: ")
+	for i := len(bin) - 1; i > 0; i-- {
+		fmt.Print(bin[i])
+	}
+	fmt.Println()
+	return decoded
 }
 
 func main() {
@@ -99,4 +227,10 @@ func main() {
 	// fmt.Println(firstPrimeNumberBefore(7))
 	// printPrime6kNumbersUntil(100)
 	// calcAbundantNumbersUntil(200)
+	// printAmicableUntil(1000000)
+	// for i := uint64(0); i < 1000000; i++ {
+	// 	checkIfArmstrongNum(i)
+	// }
+	// printPrimeFactors(420)
+	gray_decode(gray_encode(4200))
 }
