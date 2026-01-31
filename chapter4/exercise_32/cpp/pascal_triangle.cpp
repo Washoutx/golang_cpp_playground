c565195acf6c02e2f2cde4183cc2073ba5175cc3
Patryk Kups <patryk.kups.priv@gmail.com>
2026-01-31 12:56:07 +0100

    exercise 32 drop pascal triangle

---
+++ chapter4/exercise_32/cpp/pascal_triangle.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// drop pascal triangle task, waste of time
+
+int main()
+{
+
+    return 0;
+}
---
+++ chapter4/exercise_32/go/pascal_triangle.go
@@ -0,0 +1,7 @@
+package main
+
+// drop pascal triangle task, waste of time
+
+func main() {
+
+}
